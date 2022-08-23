2910e57e1de9483792beb3dbe8cf774183fdd7e2
Amalidi <A.idi12@outlook.com>
2022-08-23 13:22:31 +0100

    project files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import logo from "./logo.svg";
+import { createContext, useContext, useState } from "react";
+
 import "./App.css";
 
 function App() {
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const App = () => {
-  return <h1>Hello react</h1>;
-};
+root.render(<App />);
 
-root.render(App());
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+import { App } from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App />);
