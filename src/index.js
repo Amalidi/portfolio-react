e5c6e6c787699c9256467b6e62fe0f9bf3e911c3
Amalidi <A.idi12@outlook.com>
2022-08-23 14:28:47 +0100

    fixed error

--- src/index.js
+++ src/index.js
@@ -7,13 +7,3 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<App />);
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-
-import { App } from "./App";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(<App />);
