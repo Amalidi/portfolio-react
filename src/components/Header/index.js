e75384058cd870a9863f3b25c944a474db51f62a
Amalidi <A.idi12@outlook.com>
2022-08-26 14:21:38 +0100

    added social links

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,28 @@
 import React from "react";
 import "./Header.css";
 
+import HeaderSocials from "./HeaderSocials";
 // import TypingEffect from "./TypingEffect";
+import CTA from "./CTA";
 
 const Header = () => {
   return (
-    <header id="home">
-      <div className="form"></div>
+    <header>
+      <div className="container header_container">
+        <div className="header_text">
+          <h3>HI THERE 👋, I'M</h3>
+          <h4>Amal Idiris</h4>
+
+          {/* <TypingEffect /> */}
+
+          {/* <span> A creative problem solver.</span> */}
+
+          <p></p>
+          <CTA />
+        </div>
+
+        <HeaderSocials />
+      </div>
     </header>
   );
 };
