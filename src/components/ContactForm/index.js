e174b085cdcac98380e676bcc68788577b119ddf
Amalidi <A.idi12@outlook.com>
2022-08-27 15:38:22 +0100

    made changes on styling

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -46,7 +46,7 @@ const Contact = () => {
 
   return (
     <section id="contact">
-      <h5>Say, Hello</h5>
+      <h5>Get In Touch</h5>
       <h2>Contact</h2>
 
       <div className="container contact_container">
