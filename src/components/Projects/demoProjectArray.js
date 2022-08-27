14653fe69960d326f753a0aacd9f5dc5cca89d94
Amalidi <A.idi12@outlook.com>
2022-08-27 14:24:58 +0100

    added links

---
+++ src/components/Projects/demoProjectArray.js
@@ -0,0 +1,51 @@
+import img1 from "../../assets/code-quiz.png";
+import img2 from "../../assets/day-planner.png";
+import img3 from "../../assets/Horiseon.png";
+import img4 from "../../assets/password-generator.png";
+import img5 from "../../assets/Holiday-Project.png";
+import img6 from "../../assets/weather-dashboards.png";
+
+export const data = [
+  {
+    id: 1,
+    image: img1,
+    title: "CODE QUIZE",
+    github: "https://github.com/losier/v4",
+    demo: "http://aarabnishchal.herokuapp.com/",
+  },
+  {
+    id: 2,
+    image: img2,
+    title: "DAY PLANNER",
+    github: "https://github.com/Amalidi/Day-Planner",
+    demo: "https://amalidi.github.io/Day-Planner/",
+  },
+  {
+    id: 3,
+    image: img3,
+    title: "HORISEON CODE REFACTOR",
+    github: "https://github.com/Amalidi/Marketing-agency-refactor",
+    demo: "https://amalidi.github.io/Marketing-agency-refactor/",
+  },
+  {
+    id: 4,
+    image: img4,
+    title: "PASSWORD GENERATOR",
+    github: "https://github.com/Amalidi/-Password-Generator",
+    demo: "https://amalidi.github.io/-Password-Generator/",
+  },
+  {
+    id: 5,
+    image: img5,
+    title: "VIRTUAL HOLIDAY",
+    github: "https://github.com/C-Sim/virtual-holiday",
+    demo: "https://c-sim.github.io/virtual-holiday/index.html",
+  },
+  {
+    id: 6,
+    image: img6,
+    title: "WEATHER DASHBOARD",
+    github: "https://github.com/Amalidi/Weather-Dashboard",
+    demo: "https://amalidi.github.io/Weather-Dashboard/",
+  },
+];
